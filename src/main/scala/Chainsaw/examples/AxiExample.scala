12667d803bad0a828e736de8d86916a3fed56418
ltr <lsfans@qq.com>
2022-11-08 05:49:01 +0800

    examples of using SpinalHDL AXI4-Stream interface

---
+++ src/main/scala/Chainsaw/examples/AxiExample.scala
@@ -0,0 +1,50 @@
+package Chainsaw.examples
+
+import spinal.core._
+import spinal.core.sim._
+import spinal.lib._
+import spinal.lib.fsm._
+import spinal.lib.bus.amba4.axis._
+
+import scala.language.postfixOps
+
+object AxiExample extends App {
+
+  case class AxisFifoExample() extends Component {
+
+    val byteCount = 8
+
+    val config = Axi4StreamConfig(dataWidth = byteCount)
+
+    val clkIn, clkOut = in Bool()
+    val areset = in Bool()
+    val dataIn = slave(Axi4Stream(config))
+    val dataOut = master(Axi4Stream(config))
+
+    val domainIn = ClockDomain(clock = clkIn, reset = areset)
+    val domainOut = ClockDomain(clock = clkOut, reset = areset)
+
+    val streamFifo = StreamFifoCC(Bits(byteCount * 8 bits), 128, domainIn, domainOut)
+
+    dataIn.toBitStream() >> streamFifo.io.push // AxiStream -> Stream
+    Axi4Stream(streamFifo.io.pop) >> dataOut // Stream -> AxiStream
+  }
+
+  SpinalConfig().generateVerilog(AxisFifoExample())
+
+  case class AxisWidthAdapterExample() extends Component {
+
+    val byteCountIn = 4
+    val byteCountOut = 8
+
+    val configIn = Axi4StreamConfig(dataWidth = byteCountIn)
+    val configOut = Axi4StreamConfig(dataWidth = byteCountOut)
+
+    val dataIn = slave(Axi4Stream(configIn))
+    val dataOut = master(Axi4Stream(configOut))
+
+    val streamAdapter = Axi4StreamSimpleWidthAdapter(dataIn, dataOut)
+  }
+
+  SpinalConfig().generateVerilog(AxisWidthAdapterExample())
+}
