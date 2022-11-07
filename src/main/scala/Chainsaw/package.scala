32139562b3d1b6b6f7650676ff9e60f1b8890ffa
ltr <lsfans@qq.com>
2022-11-07 15:13:50 +0800

    add runtime variables for ChainsawGenerator

--- src/main/scala/Chainsaw/package.scala
+++ src/main/scala/Chainsaw/package.scala
@@ -1,7 +1,21 @@
 
+import org.slf4j.LoggerFactory
+import spinal.core._
+import spinal.lib._
+
+import scala.collection.mutable
+
 package object Chainsaw {
 
 
+  /** --------
+   * global run-time environment
+   * -------- */
+  val logger = LoggerFactory.getLogger("Chainsaw logger") // global logger
+  var verbose = 0
+
+  val naiveSet = mutable.Set[String]()
+  var simTime = true
 
   val dot = "■"
 
@@ -11,6 +25,18 @@ package object Chainsaw {
     def padToLeft(len: Int, elem: Char) = s.reverse.padTo(len, elem).reverse
 
     def repeat(times: Int) = Seq.fill(times)(s).reduce(_ + _)
+  }
+
+  // extension of Data
+  implicit class DataUtil[T <: Data](data: T) {
+    def d(cycle: Int = 1): T = Delay(data, cycle)
+
+  }
+
+  // extension of Bool
+  implicit class BoolUtil(data: Bool) {
+    // drive a flag which is initially unset
+    def validAfter(cycle: Int): Bool = Delay(data, cycle, init = False)
   }
 
 }
