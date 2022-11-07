394a51147c8abd2b454d418aa8f876fd2e5c4ff3
ltr <lsfans@qq.com>
2022-11-08 07:06:27 +0800

    simple DUTs for fast verification of new characteristics

---
+++ src/main/scala/Chainsaw/ChainsawDuts.scala
@@ -0,0 +1,36 @@
+package Chainsaw
+
+object ChainsawDuts {
+
+  // a pass-through
+  def simpleDut = {
+
+    val basic = Seq(-1, 0, 1, 2, 3, 4, 5, -1)
+    val frame = FrameFormat(basic, 4)
+    val formatI = frame.repeat(2).interpolate(2).pad(2)
+    val formatO = formatI
+
+    println(s"basic = $basic")
+    println(s"formatI = $formatI")
+
+    val gen: ChainsawGenerator = new ChainsawGenerator {
+      override def name = "test"
+
+      override def impl(dataIn: Seq[Any]) = dataIn
+
+      override var inputTypes = Seq.fill(4)(UIntInfo(4))
+      override var outputTypes = Seq.fill(4)(UIntInfo(4))
+
+      override var inputFormat = formatI
+      override var outputFormat = formatO
+      override var latency = 5
+
+      override def implH = new ChainsawModule(this) {
+        dataOut := dataIn.d(latency)
+      }
+    }
+
+    gen
+  }
+
+}
