9f839b59f94179a55c20fa12e019a33f5df55a24
ltr <lsfans@qq.com>
2022-11-08 09:22:58 +0800

    implement ChainsawTest and its basic test, ChainsawTest peek data according to validOut

---
+++ src/main/scala/Chainsaw/ChainsawTestTest.scala
@@ -0,0 +1,37 @@
+package Chainsaw
+
+import org.scalatest.flatspec.AnyFlatSpec
+
+import scala.util.Random
+
+class ChainsawTestTest extends AnyFlatSpec {
+
+  behavior of "ChainsawTest"
+
+  it should "pass for correct modules" in {
+
+    (0 until 20).foreach { _ =>
+      val gen = ChainsawDuts.simpleDut(correct = true)
+      val test = ChainsawTest(
+        testName = "simpleTest",
+        gen = gen,
+        data = Seq.fill(gen.inputFormat.rawDataCount * 5)(BigInt(4, Random))
+      )
+      test.doTest() // should be all true
+    }
+  }
+
+  it should "not pass for wrong modules" in {
+    val reports = (0 until 20).map { _ =>
+      val gen = ChainsawDuts.simpleDut(correct = false)
+      val test = ChainsawTest(
+        testName = "simpleTest",
+        gen = gen,
+        data = Seq.fill(gen.inputFormat.rawDataCount * 5)(BigInt(4, Random)),
+        silentTest = true
+      )
+      test.doTest()
+    }
+    assert(reports.forall(report => !report.passed)) // should be all false
+  }
+}
