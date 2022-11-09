e3eb84259d5dbcf54ce92aa29d9556996663132b
ltr <litianr@mail2.sysu.edu.cn>
2022-11-09 23:07:50 +0800

    configurable wrapper of Xillybus which is an off-the-shelf PCIe IP for both Xilinx and Altera devices(commercial)

---
+++ src/main/scala/Chainsaw/xillybus/XillybusDevice.scala
@@ -0,0 +1,29 @@
+package Chainsaw.xillybus
+
+/** a xillybus device file
+ *
+ * @param name       the name you specify through xillybus IP factory [[http://xillybus.com/custom-ip-factory]]
+ * @param deviceType = "fifo"/"mem" for stream(FIFO) or address based(RegFile or Mem) port
+ * @param direction  = "write"/"read" for write (Host -> FPGA) or read (FPGA -> Host)
+ * @param bitWidth   = 8/16/32, bit width of the fifo/register, can be
+ * @param addrWidth  for address based port, width of address
+ */
+case class XillybusDevice(name: String, deviceType: String, direction: String, bitWidth: Int, addrWidth: Int = -1) {
+
+  deviceType match {
+    case "fifo" =>
+    case "mem" => require(addrWidth != -1)
+    case _ => throw new IllegalArgumentException("device type must be fifo/mem")
+  }
+
+  val directionName = direction match {
+    case "write" => "w"
+    case "read" => "r"
+    case "bi" => ""
+    case _ => throw new IllegalArgumentException("device type must be w/r/bi")
+  }
+
+  def fullName = s"user_${directionName}_$name"
+
+  def deviceName = s"\\\\.\\xillybus_$name" // device name in windows system
+}
