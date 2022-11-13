035fe59d03c1ac2f14898fabec59cefd0715b70b
ltr <litianr@mail2.sysu.edu.cn>
2022-11-13 10:14:09 +0800

    move golden model and error bound information to TruncatedConstantMult

--- src/main/scala/Chainsaw/arithmetic/Bcm.scala
+++ src/main/scala/Chainsaw/arithmetic/Bcm.scala
@@ -9,87 +9,33 @@ import scala.language.postfixOps
 /** implement big constant multiplication by compressor tree
  *
  * @see [[Csd]]
+ * @see [[TruncatedConstantMult]]
  */
-case class Bcm(constant: BigInt, widthIn: Int, multiplierType: MultiplierType, widthTake: Int = -1, useCsd: Boolean = false)
+case class Bcm(constant: BigInt, multiplierType: MultiplierType, widthIn: Int, widthInvolved: Int, widthOut: Int, useCsd: Boolean = false)
   extends ChainsawGenerator {
+
+  val model = TruncatedConstantMult(constant, multiplierType, widthIn, widthInvolved, widthOut, useCsd)
+
+  import model._
 
   override def name = s"bcm_${constant.hashCode()}_${targetSlice.start}_until_${targetSlice.end}".replace("-", "N")
 
-  /** --------
-   * requirements
-   * -------- */
-  if (widthTake <= 0) require(multiplierType == FullMultiplier, "widthTake must be specified for LSB/MSB mode")
-
-  /** --------
-   * width calculation
-   * -------- */
-  val widthFull = constant.bitLength + widthIn
-  val targetSlice = multiplierType match {
-    case FullMultiplier => 0 until widthFull
-    case MsbMultiplier => widthFull - widthTake until widthFull
-    case LsbMultiplier => 0 until widthTake
-  }
-  val widthOut = if (multiplierType == FullMultiplier) widthFull else targetSlice.length
-  val baseWeight = targetSlice.low
-
-  /** --------
-   * I/O definition
-   * -------- */
   override var inputTypes = Seq(UIntInfo(widthIn))
   override var outputTypes = Seq.fill(2)(UIntInfo(widthOut))
 
   override var inputFormat = inputNoControl
   override var outputFormat = outputNoControl
 
-  /** --------
-   * operands construction
-   * -------- */
-  // get digits of the constant, low to high
-  val constantDigits: String = (if (useCsd) Csd(constant).csd else constant.toString(2)).reverse
-
-  logger.info(s"constant digits for BCM: $constantDigits")
-
-  val sliceAndInfos: Seq[(IndexedSeq[Int], ArithInfo)] =
-    constantDigits.zipWithIndex // digit and its weight
-      .filterNot(_._1 == '0') // skip 0s
-      .map { case (c, weight) =>
-        val range = weight until weight + widthIn
-        val inter = range intersect targetSlice // get slices by intersection, this may be empty
-        val dataSlice = inter.map(_ - weight) // inclusive slice
-        (dataSlice, ArithInfo(dataSlice.length, weight, c == '1'))
-      }
-      .filterNot(_._1.isEmpty) // skip empty slices
-      .map { case (slice, info) => (slice, info << slice.head) } // true weight
-
-  /** --------
-   * compressorTree declaration
-   * -------- */
   val compressorGen = BitHeapCompressor(sliceAndInfos.map(_._2))
   override var latency = compressorGen.latency
 
-  /** --------
-   * model & metric
-   * -------- */
   override def impl(dataIn: Seq[Any]): Seq[BigInt] = {
     val data = dataIn.asInstanceOf[Seq[BigInt]].head
-
-    // accurate model
-    val raw = sliceAndInfos.map { case (slice, info) =>
-      val segment = data.toBitValue()(slice.head to slice.last)
-      val abs = segment << info.weight
-      if (info.isPositive) abs else -abs
-    }.sum
-
-    val ret = multiplierType match {
-      case FullMultiplier => raw
-      case MsbMultiplier => raw >> baseWeight
-      case LsbMultiplier => raw % Pow2(widthTake)
-    }
-
+    val ret = model.impl(data)
     Seq(ret, BigInt(0)) // to match the output size
   }
 
-  val compensation = compressorGen.compensation >> baseWeight
+  val compensation = compressorGen.compensation >> model.widthNotOutputted
 
   val frameWiseMetric = (yours: Seq[Any], golden: Seq[Any]) => {
 
@@ -100,9 +46,9 @@ case class Bcm(constant: BigInt, widthIn: Int, multiplierType: MultiplierType, w
     val det = multiplierType match {
       case FullMultiplier => yourSum == goldenSum
       case MsbMultiplier =>
-        yourSum == goldenSum
+        (yourSum - goldenSum).abs <= 1 // introduced by compensation shift
       case LsbMultiplier =>
-        val modulus = Pow2(widthTake)
+        val modulus = Pow2(widthInvolved)
         Zp(modulus)(yourSum) == Zp(modulus)(goldenSum)
     }
 
@@ -117,29 +63,7 @@ case class Bcm(constant: BigInt, widthIn: Int, multiplierType: MultiplierType, w
 
   override val metric = ChainsawMetric(frameWise = frameWiseMetric)
 
-  /** --------
-   * error bound analysis for MSB mode
-   * -------- */
-  val widthCoeff = constantDigits.length
-  var upperBound, lowerBound, dataForUpper, dataForLower = BigInt(0)
   if (multiplierType == MsbMultiplier) {
-    (0 until widthIn).foreach { i => // accumulate the error bit by bit(low to high), as they are independent
-      val widthDropped = (baseWeight - i) min widthCoeff max 0
-      val constantDropped = // the constant which a bit should have been multiplied by (its weight)
-        if (useCsd) Csd(constantDigits).takeLow(widthDropped).evaluate
-        else constant.toBitValue().takeLow(widthDropped)
-
-      if (constantDropped >= BigInt(0)) {
-        upperBound += constantDropped << i
-        dataForUpper += BigInt(1) << i
-      } else {
-        lowerBound += constantDropped << i
-        dataForLower += BigInt(1) << i
-      }
-    }
-    upperBound = (upperBound >> baseWeight) + 1
-    lowerBound = lowerBound >> baseWeight
-
     logger.info(
       s"\n----error analysis for big constant multiplier at MSB mode----" +
         s"\n\terror bound of MSB multiplication: [$lowerBound, $upperBound]" +
@@ -157,11 +81,10 @@ case class Bcm(constant: BigInt, widthIn: Int, multiplierType: MultiplierType, w
     core.dataIn := operands.map(_.asBits)
 
     val ret = multiplierType match {
-      case MsbMultiplier => core.dataOut.map(_ >> baseWeight).map(_.resize(widthOut))
+      case MsbMultiplier => core.dataOut.map(_ >> widthNotOutputted).map(_.resize(widthOut))
       case _ => core.dataOut.map(_.resize(widthOut))
     }
 
     dataOut := ret
   }
 }
-
