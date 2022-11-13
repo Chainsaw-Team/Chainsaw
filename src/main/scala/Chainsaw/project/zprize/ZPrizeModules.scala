db4cd534cf06cfd75a14b8ffb51bcc418cfbfe9e
ltr <litianr@mail2.sysu.edu.cn>
2022-11-13 10:13:01 +0800

    move ZPrize related tests to its own package

---
+++ src/main/scala/Chainsaw/project/zprize/ZPrizeModules.scala
@@ -0,0 +1,19 @@
+package Chainsaw.project.zprize
+
+import Chainsaw.arithmetic.Bcm
+import Chainsaw.crypto.BarrettFineAlgo
+import Chainsaw.project.zprize.ZPrizeMSM.{MPrime, baseModulus}
+import Chainsaw.{LsbMultiplier, MsbMultiplier}
+
+object ZPrizeModules {
+
+  val dataWidth = baseModulus.bitLength
+  val algo = BarrettFineAlgo(baseModulus)
+  val msbWidthInvolved = algo.multMsb.widthInvolved
+  val lsbWidthInvolved = algo.multLsb.widthInvolved
+
+  def msbMultGen = Bcm(MPrime, MsbMultiplier, widthIn = dataWidth + 1, widthInvolved = msbWidthInvolved, widthOut = dataWidth + 1, useCsd = true)
+
+  def lsbMultGen = Bcm(baseModulus, LsbMultiplier, widthIn = dataWidth, widthInvolved = lsbWidthInvolved, widthOut = lsbWidthInvolved, useCsd = true)
+
+}
