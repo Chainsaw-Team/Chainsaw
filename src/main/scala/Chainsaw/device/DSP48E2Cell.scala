b6e73d11640794ff69cdf4d09aa180bff5b89cb9
cyh <1014281421@qq.com>
2022-11-13 15:31:49 +0800

    DSP for Mult added

---
+++ src/main/scala/Chainsaw/device/DSP48E2Cell.scala
@@ -0,0 +1,198 @@
+package Chainsaw.device
+
+import Chainsaw.device.MULTMODE
+import Chainsaw.device._
+import spinal.core._
+import spinal.core.sim._
+import spinal.lib.History
+
+
+/**
+ * @param nameIdx  Index for the specified dsp
+ * @param InnerDelayAB  1 for A1B1, 2 for A2B2
+ * @param ExtDelayAB  ExtraDelay for AB
+ * @param ExtDelayC  ExtraDelay for C
+ */
+// DSP48E2 Configuration for Multiply-Addition (A * B + C)
+class DSP48E2Cell_MultAdd(nameIdx: Int, InnerDelayAB:Int = 1, ExtDelayAB: Int = 0, ExtDelayC: Int = 1) extends Component {
+  val io = new Bundle {
+    // 直接输入输出信号
+    val A = in  UInt(30 bits)
+    val B = in  UInt(18 bits)
+    val C = in  UInt(48 bits)
+    val P = out UInt(48 bits)
+    val PATTERNDETECT = out Bool()    // For Pattern Recognition
+  }
+  noIoPrefix()
+
+  val attr = DSPAttrBuilder().setMult(MULTMODE.AB1).build    // AB1 for A * B
+  // TODO: pay attention to here
+  attr.AREG          = 2    // A1: AREG = 2 or INMODE[0] = 1;  A2: AREG = 1 or 2 and INMODE[0] = 0
+  attr.BREG          = 2    // B1: BREG = 2 or INMODE[4] = 1;  B2: BREG = 1 or 2 and INMODE[4] = 0
+  attr.ACASCREG      = 1
+  attr.BCASCREG      = 1
+  attr.DREG          = 1
+  attr.ADREG         = 1
+  attr.INMODEREG     = 1
+  attr.ALUMODEREG    = 1
+  attr.OPMODEREG     = 1
+  attr.CARRYINREG    = 1
+  attr.CARRYINSELREG = 1
+  attr.PREG          = 1
+  attr.AMULTSEL      = "A"
+  attr.BMULTSEL      = "B"
+  attr.PREADDINSEL   = "A"
+  attr.USE_MULT      = "MULTIPLY"
+  attr.MREG          = 1
+  // TODO: 模式识别的配置: 检测46号bit
+  attr.MASK          = B"101111111111111111111111111111111111111111111111"    // 48bits, 置1的bit被忽略
+  attr.PATTERN       = B"010000000000000000000000000000000000000000000000"    // 48bits, 想要匹配的模式
+  attr.SEL_MASK      = "MASK"
+  attr.SEL_PATTERN   = "PATTERN"
+  attr.USE_PATTERN_DETECT = "PATDET"
+
+  val dsp = new DSP48E2(attr).setName(s"dsp_MultAdd_${nameIdx}")
+  dsp.RSTs.all.foreach(_.clear())
+  dsp.CEs.all.foreach(_.set())
+
+  // 对信号的延迟
+  val delayedA = History(io.A, ExtDelayAB+1, init=U(0, 30 bits))    // shift registers
+  val lastA = delayedA(delayedA.length-1)
+
+  val delayedB = History(io.B, ExtDelayAB+1, init=U(0, 18 bits))    // shift registers
+  val lastB = delayedB(delayedB.length-1)
+
+  val delayedC = History(io.C, ExtDelayC+1, init=U(0, 48 bits))    // shift registers
+  val lastC = delayedC(delayedC.length-1)
+
+  // ******** 乘法加法: A * B + C ******** //
+  // 直接输入
+  dsp.DATAIN.A       := lastA          // link to the end of shift registers
+  dsp.DATAIN.B       := lastB          // link to the end of shift registers
+  dsp.DATAIN.C       := lastC          // link to the end of shift registers
+  dsp.DATAIN.D       := U(0)
+  dsp.DATAIN.CARRYIN := False          // TODO: consider later
+
+  // 级联输入置零
+  dsp.CASCDATAIN.A        := U(0)
+  dsp.CASCDATAIN.B        := U(0)
+  dsp.CASCDATAIN.P        := U(0)
+  dsp.CASCDATAIN.CARRY    := False    // TODO: consider later
+  dsp.CASCDATAIN.MULTSIGN := False    // TODO: consider later
+
+  // 控制信号设置
+  dsp.INST.ALUMODE    := B"0000"
+  dsp.INST.OPMODE     := B"110000101"
+  dsp.INST.CARRYINSEL := B"000"
+
+  //dsp.INST.INMODE := B"00000"    // for A2B2
+
+  if (InnerDelayAB==1) {
+    dsp.INST.INMODE := B"10001"    // for A1B1
+  } else if (InnerDelayAB==2) {
+    dsp.INST.INMODE := B"00000"    // for A2B2
+  } else {
+    dsp.INST.INMODE := B"10001"
+  }
+
+  // 直接输出
+  io.P := dsp.DATAOUT.P
+
+  // 模式识别输出
+  io.PATTERNDETECT := dsp.DATAOUT.PATTERNDETECT    // TODO: 验证一下这个
+
+}
+
+
+object DSP48E2Cell_MultAdd_Main {
+  def main(args: Array[String]) {
+    //NewSpinalConfig.generateVerilog(new DSP48E2Cell_MultAdd(0, 1, 0,1)).printRtl()
+    NewSpinalConfig.generateVerilog(new DSP48E2Cell_MultAdd(0, 2, 0,2)).printRtl()
+  }
+}
+
+
+
+// 加法乘法减法: (A + D) * B - C
+class DSP48E2Cell_AddMultMinus(nameIdx: Int, ExtDelayABD: Int = 0, ExtDelayC: Int = 1) extends Component {
+  val io = new Bundle {
+    // 直接输入输出信号
+    val A = in  UInt (30 bits)
+    val B = in  UInt (18 bits)
+    val C = in  UInt (48 bits)
+    val D = in  UInt (27 bits)
+    val P = out UInt (48 bits)
+  }
+  noIoPrefix()
+
+  val attr = DSPAttrBuilder().setMult(MULTMODE.AD0B1).build    // AD0B1 for (A + D) * B
+  // TODO: pay attention to here
+  attr.AREG = 1        // A1: AREG = 2 and INMODE[0] = 1, or AREG = 1 and INMODE[0] = 0;  A2: AREG = 2 and INMODE[0] = 0
+  attr.BREG = 2        // B1: BREG = 2 and INMODE[4] = 1, or BREG = 1 and INMODE[4] = 0;  B2: BREG = 2 and INMODE[4] = 0
+  attr.ACASCREG = 1
+  attr.BCASCREG = 1
+  attr.DREG = 1
+  attr.ADREG = 1                // for ADREG
+  attr.INMODEREG = 1
+  attr.ALUMODEREG = 1
+  attr.OPMODEREG = 1
+  attr.CARRYINREG = 1
+  attr.CARRYINSELREG = 1
+  attr.PREG = 1
+  attr.AMULTSEL = "AD"          // "A" or "AD"
+  attr.BMULTSEL = "B"           // "B" or "AD"
+  attr.PREADDINSEL = "A"        // "A" or "B"
+  attr.USE_MULT = "MULTIPLY"
+  attr.MREG = 1
+
+  val dsp = new DSP48E2(attr).setName(s"dsp_AddMultMinus_${nameIdx}")
+  dsp.RSTs.all.foreach(_.clear())
+  dsp.CEs.all.foreach(_.set())
+
+  // The delaying of signals
+  val delayedA = History(io.A, ExtDelayABD+1, init=U(0, 30 bits))    // shift registers
+  val lastA = delayedA(delayedA.length-1)
+
+  val delayedB = History(io.B, ExtDelayABD+1, init=U(0, 18 bits))    // shift registers
+  val lastB = delayedB(delayedB.length-1)
+
+  val delayedC = History(io.C, ExtDelayC+1, init=U(0, 48 bits))      // shift registers
+  val lastC = delayedC(delayedC.length-1)
+
+  val delayedD = History(io.D, ExtDelayABD+1, init=U(0, 27 bits))    // shift registers
+  val lastD = delayedD(delayedD.length-1)
+
+  // ******** 加法乘法减法: (A + D) * B - C ******** //
+  // 直接输入
+  dsp.DATAIN.A       := lastA          // link to the end of shift registers
+  dsp.DATAIN.B       := lastB          // link to the end of shift registers
+  dsp.DATAIN.C       := lastC          // link to the end of shift registers
+  dsp.DATAIN.D       := lastD          // link to the end of shift registers
+  dsp.DATAIN.CARRYIN := False          //
+
+  // 级联输入置零
+  dsp.CASCDATAIN.A        := U(0)
+  dsp.CASCDATAIN.B        := U(0)
+  dsp.CASCDATAIN.P        := U(0)
+  dsp.CASCDATAIN.CARRY    := False     //
+  dsp.CASCDATAIN.MULTSIGN := False     //
+
+  // 控制信号设置
+  dsp.INST.ALUMODE    := B"0001"       // - Z + (W + X + Y + CIN) - 1
+  dsp.INST.OPMODE     := B"000110101"  // W=0, Z=C, X=Y=M
+  dsp.INST.INMODE     := B"00100"      // for A1,D1,AD,B2
+  dsp.INST.CARRYINSEL := B"001"        // Important
+
+  // 直接输出
+  io.P := dsp.DATAOUT.P
+
+}
+
+
+object DSP48E2Cell_AddMultAdd_Main {
+  def main(args: Array[String]) {
+    NewSpinalConfig.generateVerilog(new DSP48E2Cell_AddMultMinus(0,0,2)).printRtl()
+  }
+}
+
+object NewSpinalConfig extends SpinalConfig(defaultConfigForClockDomains = ClockDomainConfig(resetKind = SYNC))
---
+++ src/test/scala/Chainsaw/arithmetic/KaraBaseTest.scala
@@ -0,0 +1,23 @@
+package Chainsaw.arithmetic
+
+import Chainsaw._
+import org.scalatest.flatspec.AnyFlatSpec
+
+import scala.util.Random
+
+class KaraBaseTest extends AnyFlatSpec {
+
+  def testKaraBase(mode: MultiplierType) = {
+    val widthA = 8
+    val widthB = if (mode == Kara) 10 else 8
+    val data = Seq.fill(1000)(Seq(BigInt(widthA, Random), BigInt(widthA, Random), BigInt(widthB, Random), BigInt(widthB, Random))).flatten
+    val gen = KaraBase(widthA, widthB, mode)
+    ChainsawTest("testKaraBase", gen, data).doTest()
+  }
+
+  //  val modes = Seq(FullMultiplier, SquareMultiplier, MsbMultiplier, LsbMultiplier, Kara)
+  val modes = Seq(FullMultiplier, Kara)
+
+  "kara base" should "work" in modes.foreach(testKaraBase)
+
+}
