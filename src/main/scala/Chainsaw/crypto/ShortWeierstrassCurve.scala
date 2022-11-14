545616d5e24d74fbe9cb0d010fa9d4017f81421e
ZhizunDoubi <1411203632@qq.com>
2022-11-14 22:29:28 +0800

    pladder

--- src/main/scala/Chainsaw/crypto/ShortWeierstrassCurve.scala
+++ src/main/scala/Chainsaw/crypto/ShortWeierstrassCurve.scala
@@ -1,5 +1,6 @@
 package Chainsaw.crypto
 
+import Chainsaw._
 import cc.redberry.rings.scaladsl._
 import cc.redberry.rings.scaladsl.syntax._
 
@@ -33,6 +34,7 @@ case class ShortWeierstrassCurve(override val modulus: IntZ, override val c: Int
     val a12 = zp.add(m01, m02) // y1y2 + z1z2
     val m10 = zp.multiply(a00, a01) // x1x2 + y1y2 + x1y2 + x2y1
     val m11 = zp.multiply(a02, a03) // x1x2 + z1z2 + x1z2 + x2z1
+
     val m12 = zp.multiply(a04, a05) // y1y2 + z1z2 + y1z2 + y2z1
     // stage 2
     val tri20 = m02 * 3 // 3z1z2
@@ -67,4 +69,51 @@ case class ShortWeierstrassCurve(override val modulus: IntZ, override val c: Int
   override def padd(p0: EcPointAffine, p1: EcPointAffine) = {
     paddHomo(p0.toProjective, p1.toProjective).toAffine(Homo)
   }
+
+  def pladder(XQP: IntZ, XRP: IntZ, M: IntZ, YP: IntZ): (IntZ, IntZ, IntZ, IntZ) = {
+    val zp = Zp(modulus)
+
+    val YRBar = zp.add(YP, zp.multiply(2, M, XRP))
+    val E = zp.subtract(XQP, XRP)
+    val F = zp.multiply(YRBar, E)
+    val G = zp.multiply(E, E)
+    val XRPDot = zp.multiply(XRP, G)
+    val H = zp.multiply(YRBar, YRBar)
+    val MDot = zp.multiply(M, F)
+    val YPDot = zp.multiply(YP, F, G)
+    val K = zp.add(H, MDot)
+    val L = zp.add(K, MDot)
+    val MDotDot = zp.subtract(XRPDot, K)
+    val XSP = zp.multiply(H, L)
+    val XTP = zp.add(zp.multiply(XRPDot, XRPDot), YPDot)
+    val YPDotDot = zp.multiply(YPDot, H)
+
+    (XSP, XTP, MDotDot, YPDotDot)
+  }
+
+  override def pmult(k: IntZ, p: EcPointAffine): EcPointAffine = {
+    val zp = Zp(modulus)
+
+    val Z2 = zp.multiply(4, p.y, p.y)
+    var M = zp.add(zp.multiply(3, p.x, p.x), c)
+    var XQP: IntZ = 0
+    var XRP = zp.subtract(zp.multiply(M, M), zp.multiply(3, p.x, Z2))
+    var YP = zp.multiply(Z2, Z2)
+
+    k.toBigInt.toString(2).tail.foreach { bit =>
+      val pair = if (bit.asDigit == 1) pladder(XQP, XRP, M, YP) else pladder(XRP, XQP, M, YP)
+      XQP = if (bit.asDigit == 1) pair._1 else pair._2
+      XRP = if (bit.asDigit == 1) pair._2 else pair._1
+      M = pair._3
+      YP = pair._4
+    }
+
+    val XP = zp.divideExact(zp.subtract(zp.subtract(zp.multiply(M, M), XQP), XRP), 3)
+    val Z = zp.divideExact(zp.divideExact(zp.multiply(p.x, YP), 2), zp.multiply(p.y, XP))
+
+    val ret = EcPointProj(zp.add(XP, XQP), zp.divideExact(zp.add(YP, zp.multiply(2, M, XQP)), 2), Z).toAffine(Jacobi)
+    assert(isOnCurve(ret) && ret == multBase(k, p), s"pmult failed on $k * $p")
+    ret
+  }
+
 }
