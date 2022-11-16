cfb83252fbdb91658fb4cf0119c3104cfa0c0d67
lqx <liqx36@mail2.sysu.edu.cn>
2022-11-16 16:02:30 +0800

    Update the cpa code

--- src/test/scala/Chainsaw/arithmetic/CpaTest.scala
+++ src/test/scala/Chainsaw/arithmetic/CpaTest.scala
@@ -6,34 +6,22 @@ import org.scalatest.flatspec.AnyFlatSpec
 import scala.util.Random
 
 class CpaTest extends AnyFlatSpec {
-
   behavior of "Cpa Mode Test"
 
-  def testCpaMode(mode: CpaMode): Unit = {
-    it should s"work correctly on ${mode.getClass.getSimpleName}" in {
-      val data = Seq.fill(1000)(BigInt(10, Random))
-      val gen = Cpa(BinaryAdder, Seq.fill(4)(10), mode, withCarry = true)
-      ChainsawTest("testAdd", gen, data).doTest()
-    }
-  }
-
-  Seq(M2S, S2M, S2S, M2M).foreach(testCpaMode)
-
-  behavior of "adder Mode Test"
-
-  def testAdderMode(mode: AdderType): Unit = {
-    it should s"work correctly on ${mode.getClass.getSimpleName}" in {
+  def testCpaFuncInMode(adder: AdderType, mode: CpaMode, useNaive: Boolean = false): Unit = {
+    it should s"work correctly on ${adder.getClass.getSimpleName.init} by ${mode.getClass.getSimpleName.init}" in {
       val data = Seq.fill(1200)(BigInt(10, Random))
-      val gen0 = Cpa(mode, Seq.fill(4)(10), S2S, withCarry = true)
-      val gen1 = Cpa(mode, Seq.fill(4)(10), S2S, withCarry = false)
-      ChainsawTest("testCpa", gen0, data).doTest()
-      ChainsawTest("testCpa", gen1, data).doTest()
+      val gen0 = Cpa(adder, Seq.fill(4)(10), mode, withCarry = true)
+      val gen1 = Cpa(adder, Seq.fill(4)(10), mode, withCarry = false)
+      // use Naive impl
+      if (useNaive) Seq(gen0, gen1).foreach(_.setAsNaive())
+      ChainsawTest(s"testCpa_${if (useNaive) "useNaive" else "useImplH"}_withCarry", gen0, data).doTest()
+      ChainsawTest(s"testCpa_${if (useNaive) "useNaive" else "useImplH"}_withoutCarry", gen1, data).doTest()
     }
   }
 
-  Seq(BinaryAdder, BinarySubtractor,
-    //    TernaryAdder, TernarySubtractor1, TernarySubtractor2 // TODO: implementation of ternary adder
-  ).foreach(testAdderMode)
+  val adderTypes = Seq(BinaryAdder, BinarySubtractor, TernaryAdder, TernarySubtractor1, TernarySubtractor2)
+  val cpaModes   = Seq(M2M, M2S, S2M, S2S)
+  cpaModes.foreach { mode => adderTypes.foreach(adder => testCpaFuncInMode(adder, mode, useNaive = false)) }
 
-  // TODO: a high dimensional matrix test for all configurations
 }
