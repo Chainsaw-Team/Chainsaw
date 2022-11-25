6c345c85917f3874e87abeba88faf8499c1e011c
ltr <litianr@mail2.sysu.edu.cn>
2022-11-25 12:01:28 +0800

    implement DynamicCounter, need verification fix some problems in NumericType

---
+++ src/main/scala/Chainsaw/memory/DynamicCounter.scala
@@ -0,0 +1,40 @@
+package Chainsaw.memory
+
+import spinal.core._
+import spinal.core.sim._
+import spinal.lib._
+import spinal.lib.fsm._
+
+// TODO: verification for dynamic counter
+class DynamicCounter(end: UInt) extends ImplicitArea[UInt] {
+
+  val willIncrement = False.allowOverride
+  val willClear = False.allowOverride
+
+  def clear(): Unit = willClear := True
+
+  def increment(): Unit = willIncrement := True
+
+  def set(value:UInt): Unit = valueNext := value
+
+  val width = end.getBitsWidth
+
+  val valueNext = cloneOf(end)
+  val value = RegNext(valueNext) init valueNext.getZero
+  val willOverflowIfInc = value === end - U(1)
+  val willOverflow = willOverflowIfInc && willIncrement
+
+  when(willOverflow) {
+    valueNext := U(0)
+  } otherwise {
+    valueNext := (value + U(willIncrement)).resized
+  }
+  when(willClear) {
+    valueNext := 0
+  }
+
+  willOverflowIfInc.allowPruning()
+  willOverflow.allowPruning()
+
+  override def implicitValue = value
+}
