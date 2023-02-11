7b7f1c74501f0fe31ad359a98dda546ff80f0d72
ltr <lsfans@qq.com>
2023-02-11 17:05:13 +0800

    add methods for Scala case class <-> Python Dict

--- src/main/scala/Chainsaw/io/pythonIo.scala
+++ src/main/scala/Chainsaw/io/pythonIo.scala
@@ -3,9 +3,10 @@ package Chainsaw.io
 import ai.djl.ndarray._
 import spinal.core._
 
-import java.io.{BufferedReader, File, InputStreamReader}
+import java.io.{BufferedReader, File, InputStreamReader, PrintWriter}
 import java.nio.file.{Files, Paths}
 import scala.collection.mutable.ArrayBuffer
+import scala.io.Source
 import scala.language.implicitConversions
 
 object pythonIo {
@@ -21,6 +22,7 @@ object pythonIo {
 
   val inputArrayFile  = new File(pythonUtilsPath, "input.npz")
   val outputArrayFile = new File(pythonUtilsPath, "output.npz")
+  val configFile      = new File(pythonUtilsPath, "config.json")
 
   def exportSignal(yours: Signal*): File = {
     val manager = NDManager.newBaseManager()
@@ -43,6 +45,31 @@ object pythonIo {
       .map(decoded.get)
       .map(_.toDoubleArray.map(BigDecimal(_)).toSeq)
     ret
+  }
+
+  import org.json4s._
+  import org.json4s.jackson.JsonMethods._
+  import org.json4s.jackson.Serialization.{read, write}
+
+  /** import serialized python dict for config parameters
+    * @param file
+    * @tparam T
+    * @return
+    */
+  // tutorial: https://queirozf.com/entries/json4s-examples-common-basic-operations-using-jackson-as-backend
+  def importConfig[T: Manifest](file: File = configFile) = {
+    val src              = Source.fromFile(file)
+    val lines            = src.getLines().mkString("\n")
+    implicit val formats = DefaultFormats
+    parse(lines).extract[T]
+  }
+
+  def exportConfig[T <: AnyRef](config: T) = {
+    implicit val formats = DefaultFormats
+    val lines            = write[T](config)
+    val pw               = new PrintWriter(configFile)
+    pw.write(lines)
+    pw.close()
   }
 
   def runPython(pyPath: File, args: String*): String = {
@@ -75,6 +102,17 @@ object pythonIo {
     exportSignal(signal: _*)
     runPython(pyPath, args)
     importSignal()
+  }
+
+  def main(args: Array[String]): Unit = {
+    case class Config(
+        name: String,
+        pulse_points: Int,
+        gauge_points: Int,
+        data: Array[Int]
+    )
+    exportConfig(Config("config", 50, 50000, Array(1, 2, 3, 4)))
+    println(importConfig[Config]().data.mkString(" "))
   }
 
 }
