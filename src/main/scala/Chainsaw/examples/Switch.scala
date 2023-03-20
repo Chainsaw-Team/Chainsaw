a924d6afd0ebf3773c01abf8918474599e4d004c
ltr <litianr@mail2.sysu.edu.cn>
2023-03-20 18:55:44 +0800

    implement switch

---
+++ src/main/scala/Chainsaw/examples/Switch.scala
@@ -0,0 +1,56 @@
+package Chainsaw.examples
+
+import spinal.core._
+import spinal.lib._
+import spinal.core.sim._
+import spinal.sim._
+import spinal.lib.fsm._
+import spinal.lib.bus._
+
+import Chainsaw._
+import Chainsaw.xilinx._
+case class Switch() extends ChainsawOperatorGenerator with FixedLatency {
+  override def implH: ChainsawOperatorModule = new ChainsawOperatorModule(this) {
+    val Seq(a, b)         = dataIn
+    val Seq(aOut, bOut)   = dataOut
+    def getIndex(x: AFix) = x.asBits.takeHigh(8).asUInt
+    val det               = getIndex(a) > getIndex(b)
+    aOut := Mux(det, a, b)
+    bOut := Mux(det, b, a)
+  }
+
+  override def implNaiveH: Option[ChainsawOperatorModule] = ???
+
+  override def name: String = "Switch"
+
+  /** -------- performance
+    * --------
+    */
+  override def vivadoUtilEstimation: VivadoUtil = ???
+
+  override def fmaxEstimation: HertzNumber = ???
+
+  /** -------- interfaces
+    * --------
+    */
+  override def inputTypes: Seq[NumericType] = Seq.fill(2)(NumericType.U(16))
+
+  override def outputTypes: Seq[NumericType] = Seq.fill(2)(NumericType.U(16))
+
+  /** -------- model
+    * --------
+    */
+  override def impl(testCase: TestCase): Seq[BigDecimal] = ???
+
+  override def metric(yours: Seq[BigDecimal], golden: Seq[BigDecimal]): Boolean = ???
+
+  override def testCases: Seq[TestCase] = ???
+
+  override def latency(): Int = 0
+}
+
+object Switch {
+  def main(args: Array[String]): Unit = {
+    ChainsawImpl(Switch())
+  }
+}
